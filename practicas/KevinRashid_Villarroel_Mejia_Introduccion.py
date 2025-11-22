1d47719e520663e0288a820ca5d92dd6af76046b
kevin.rashiid <kevin.villaroelm@gmail.com>
2025-11-22 18:51:10 +0100

    otros

---
+++ practicas/KevinRashid_Villarroel_Mejia_Introduccion.py
@@ -0,0 +1,10 @@
+
+
+print("····CALCULAMOS EL AREA DE UN RECTANGULO····")
+
+base=float(input("Introduce base del rectangulo "))
+
+altura=float(input("Introduce altura del rectangulo "))
+
+print("La base del rectangulo es ",base*altura)
+
