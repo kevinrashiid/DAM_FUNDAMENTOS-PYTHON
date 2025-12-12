217aa6194b938970e14ae7396fec0618885020e6
kevin <kevin.villaroel@educa.madrid.org>
2025-12-12 10:17:03 +0100

    OTROSs

---
+++ Boletin7/B1_1.py
@@ -0,0 +1,20 @@
+'''1. Escribe un programa que dadas dos tuplas diferentes nos cree otra con los elementos comunes a
+ambas ordenada de forma ascendente y sin repeticiones. Ejemplo:
+tupla1 = (1,4,2,5,49,3,75,3)
+tupla2 = (3,3,75, 75, 180, 9, 5)
+La salida debería de ser así:
+Los elementos repetidos en ambas son: (3,5,75)'''
+
+tupla1 = (1,4,2,5,49,3,75,3)
+tupla2 = (3,3,75, 75, 180, 9, 5)
+
+listaAux=[] ##lista vacia
+for i in tupla1:
+    for j in tupla2:
+        ##TODO si la i es igual a j Y la i no esta en la listaAux lo añada
+        if i==j and i not in listaAux:
+            listaAux.append(i)
+listaAux.sort() ##ordenando
+listaAux=tuple(listaAux)##TODO reasignamos la lista y la casteamos a tuple
+print(listaAux)
+
---
+++ Boletin7/B1_2.py
@@ -0,0 +1,21 @@
+
+
+'''2. Modifica ahora el programa anterior para que cree una tupla con los elementos que están en una u
+otra pero no en las dos. Muéstrala ahora ordenados descendentemente. Con el mismo ejemplo del
+caso anterior la salida sería esta:
+Los elementos que sólo aparecen en una son: (180,49,9,4,2,1'''
+tupla1 = (1,4,2,5,49,3,75,3)
+tupla2 = (3,3,75, 75, 180, 9, 5)
+
+listaAux=[] ##lista vacia
+for i in tupla1:
+    if i not in tupla2 and i not in listaAux:
+        listaAux.append(i)
+for i in tupla2:
+    if i not in tupla2 and i not in listaAux:
+        listaAux.append(i)
+
+listaAux.sort(reverse=True) ##ordenando al reves
+
+listaAux=tuple(listaAux)##TODO reasignamos la lista y la casteamos a tuple
+print(listaAux)
