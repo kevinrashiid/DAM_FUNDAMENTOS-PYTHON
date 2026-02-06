59d7b33e2a16066ea015200cc2855f85b5e39f70
kevin <kevin.villaroel@educa.madrid.org>
2026-02-06 14:07:38 +0100

    OTROSs

---
+++ boletin6/ejercicio4Excepciones.py
@@ -0,0 +1,29 @@
+from itertools import count
+
+def fraccion(fraccion):
+    resultado=0.0
+    try:
+        numerador,denominador=fraccion.split("/")#PONE EL / COMO SEPARADOR
+        resultado= int(numerador)/int(denominador)
+    except :
+        print("El numero no es un numerador")
+    finally: #LO DEVUELVE SI O SI
+        return resultado
+
+
+    """
+    if fraccion.count("/")!=1: #SI EL CONTEO DE "/" ES DIFERENTE A 1 DEVUELVE 0
+        return 0.0
+    else:
+        numerador,denominador=fraccion.split("/")#PONE EL / COMO SEPARADOR
+        if not numerador.isnumeric() or not denominador.isnumeric(): # SI EL PRIMER NUMERO Y EL SEGUNDO NO ES NUMERICO DEVUELVE 0
+            return 0.0
+        return int(numerador)/int(denominador) #DEVUELVE EL RESULTADO DE LA OPERACION
+    """
+print(fraccion("25/10"))
+print(fraccion("a/10"))
+print(fraccion("10//a"))
+print(fraccion("//10"))
+print(fraccion("10"))
+
+
